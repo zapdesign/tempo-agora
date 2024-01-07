8b8d5c3a7c2335f55c52ed4f16767cc46ac28802
giuseppe <giuseppegabriel4@gmail.com>
2024-01-06 21:20:31 -0300

    Atualizada barra de pesquisa

--- js/elementos.js
+++
@@ -1,8 +0,0 @@
-const tempMax = document.getElementById('temp-max')
-const tempMin = document.getElementById('temp-min')
-const sensacao = document.getElementById('sensacao')
-const tempo = document.getElementById('temp-princ')
-const descricao = document.getElementById('descricao')
-
-
-export {tempMax, tempMin, sensacao, tempo, descricao}
